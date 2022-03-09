5f02db01c5eec31b1852085b36961ac05c5f8b7d
pablovicho <pablovvb@gmail.com>
2022-03-08 23:46:07 -0600

    js done, and most styles. Three sections left

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const finalDate= new Date(2022, 10, 13);
+const now = new Date();
+const domDays= document.getElementById('daysLeft')
+
+let time_difference = finalDate.getTime() - now.getTime();    
+         //calcular los días dividiendo los milisegundos en un  día  
+let days_difference = parseInt(time_difference / (1000 * 60 * 60 * 24));  
+
+domDays.innerText=`Quedan ${days_difference} días`
